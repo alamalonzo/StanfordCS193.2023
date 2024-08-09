862411195f1778077c2126f842fdaf379614fbb0
Alam <alonzo.alam@hotmail.com>
2024-08-09 10:58:29 -0400

    Initial Commit

---
+++ StanfordCS193.2023/StanfordCS193_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCS193_2023App.swift
+//  StanfordCS193.2023
+//
+//  Created by Alam Alonzo on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCS193_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
